33964f1c4fb4951b6df60bcfe7c7f50d025ce7fc
Rani2000 <103360448+Rani2000@users.noreply.github.com>
2023-10-04 17:31:55 +0530

    test

--- js/script.js
+++ js/script.js
@@ -11,117 +11,208 @@ const updateToast = document.getElementById("update-toast");
 const deleteToast = document.getElementById("delete-toast");
 
 form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    addTodo(item.value);
-    item.value = "";
-    showaddmsg();
-})
+  e.preventDefault();
+  addTodo(item.value);
+  item.value = "";
+  showaddmsg();
+});
 
 const saveTodo = () => {
-    const alltodos = document.querySelectorAll('li p');
-    const data = [];
-    alltodos.forEach((todos) => {
-        data.push(todos.innerHTML);
-    })
-    localStorage.setItem("todos", JSON.stringify(data));
-    if (data.length == 0) {
-        localStorage.removeItem("todos");
-    }
-}
+  const alltodos = document.querySelectorAll("li p");
+  const data = [];
+  alltodos.forEach((todos) => {
+    data.push(todos.innerHTML);
+  });
+  localStorage.setItem("todos", JSON.stringify(data));
+  if (data.length == 0) {
+    localStorage.removeItem("todos");
+  }
+};
 
 const addTodo = (value) => {
-    const listItem = document.createElement("li");
-    listItem.setAttribute("draggable", "true");
-    listItem.addEventListener('drag', (e) => {
-        list = e.target;
-    })
-    completeTask.addEventListener("dragover", (e) => {
-        e.preventDefault();
-    })
-    todoTask.addEventListener("dragover", (e) => {
-        e.preventDefault();
-    })
-    completeTask.addEventListener("drop", (e) => {
-        completeTaskLists.append(list);
-        list.classList.add('done');
-    })
-    todoTask.addEventListener("drop", (e) => {
-        todoTaskLists.append(list);
-        list.classList.remove('done');
-    })
-    listItem.innerHTML = `
+  const listItem = document.createElement("li");
+  listItem.setAttribute("draggable", "true");
+  listItem.addEventListener("drag", (e) => {
+    list = e.target;
+  });
+  completeTask.addEventListener("dragover", (e) => {
+    e.preventDefault();
+  });
+  todoTask.addEventListener("dragover", (e) => {
+    e.preventDefault();
+  });
+  completeTask.addEventListener("drop", (e) => {
+    completeTaskLists.append(list);
+    list.classList.add("done");
+  });
+  todoTask.addEventListener("drop", (e) => {
+    todoTaskLists.append(list);
+    list.classList.remove("done");
+  });
+  listItem.innerHTML = `
         <p>${value}</p>
         <div class="icon">
             <i class="fas fa-solid fa-pen"></i>
             <i class="fas fa-times"></i>
         </div>      
     `;
-    todoTaskLists.append(listItem);
-    listItem.querySelector('.fa-times').addEventListener('click', function () {
-        listItem.remove();
-        saveTodo();
-        showdeletemsg();
-    })
-    listItem.querySelector('.fa-pen').addEventListener('click', function () {
-        document.body.style.overflow = 'hidden'
-        updateTask.style.display = "block";
-        updateTask.querySelector('input').value = listItem.querySelector('p').innerHTML;
-        updateTask.querySelectorAll('button')[0].addEventListener('click', function () {
-            listItem.querySelector('p').innerHTML = updateTask.querySelector('input').value;
-            showupdatemsg();
-            updateTask.style.display = "none";
-            document.body.style.overflow = 'auto';
-        })
-        updateTask.querySelectorAll('button')[1].addEventListener('click', function () {
-            updateTask.style.display = "none";
-            document.body.style.overflow = 'auto';
-        })
-        updateTask.querySelector('i').addEventListener('click', function () {
-            updateTask.style.display = "none";
-            document.body.style.overflow = 'auto';
-        })
-    })
+  todoTaskLists.append(listItem);
+  listItem.querySelector(".fa-times").addEventListener("click", function () {
+    listItem.remove();
     saveTodo();
-}
+    showdeletemsg();
+  });
+  listItem.querySelector(".fa-pen").addEventListener("click", function () {
+    document.body.style.overflow = "hidden";
+    updateTask.style.display = "block";
+    updateTask.querySelector("input").value =
+      listItem.querySelector("p").innerHTML;
+    // updateTask
+    //   .querySelectorAll("button")[0]
+    //   .addEventListener("click", function () {
+    //     listItem.querySelector("p").innerHTML =
+    //       updateTask.querySelector("input").value;
+    //     showupdatemsg();
+    //     updateTask.style.display = "none";
+    //     document.body.style.overflow = "auto";
+    //   });
+    // updateTask
+    //   .querySelectorAll("button")[1]
+    //   .addEventListener("click", function () {
+    //     updateTask.style.display = "none";
+    //     document.body.style.overflow = "auto";
+    //   });
+    function yes() {
+      listItem.querySelector("p").innerHTML =
+        updateTask.querySelector("input").value;
+      updateTask.style.display = "none";
+      document.body.style.overflow = "auto";
+      updateTask
+        .querySelectorAll("button")[0]
+        .removeEventListener("click", yes);
+      updateTask.querySelectorAll("button")[1].removeEventListener("click", no);
+    }
+    function no() {
+      updateTask.style.display = "none";
+      document.body.style.overflow = "auto";
+      updateTask
+        .querySelectorAll("button")[0]
+        .removeEventListener("click", yes);
+      updateTask.querySelectorAll("button")[1].removeEventListener("click", no);
+    }
+    updateTask.querySelectorAll("button")[0].addEventListener("click", yes);
+    updateTask.querySelectorAll("button")[1].addEventListener("click", no);
+    updateTask.querySelector("i").addEventListener("click", function () {
+      updateTask.style.display = "none";
+      document.body.style.overflow = "auto";
+    });
+  });
+  saveTodo();
+};
+
+// function deleteItem(listItem) {
+//   listItem.querySelector(".fa-times").addEventListener("click", function () {
+//     deleteTask.style.display = "block";
+//     document.body.style.overflow = "hidden";
+//     function yes() {
+//       deleteTask.style.display = "none";
+//       document.body.style.overflow = "auto";
+//       listItem.remove();
+//       toastMsg("Tasks is successfully deleted");
+//       deleteTask
+//         .querySelectorAll("button")[0]
+//         .removeEventListener("click", yes);
+//       deleteTask.querySelectorAll("button")[1].removeEventListener("click", no);
+//     }
+//     function no() {
+//       deleteTask.style.display = "none";
+//       document.body.style.overflow = "auto";
+//       deleteTask
+//         .querySelectorAll("button")[0]
+//         .removeEventListener("click", yes);
+//       deleteTask.querySelectorAll("button")[1].removeEventListener("click", no);
+//     }
+//     deleteTask.querySelectorAll("button")[0].addEventListener("click", yes);
+//     deleteTask.querySelectorAll("button")[1].addEventListener("click", no);
+//     deleteTask.querySelector("i").addEventListener("click", function () {
+//       deleteTask.style.display = "none";
+//       document.body.style.overflow = "auto";
+//     });
+//   });
+// }
+
+// function updateItem(listItem) {
+//   listItem.querySelector(".fa-pen").addEventListener("click", function () {
+//     document.body.style.overflow = "hidden";
+//     updateTask.style.display = "block";
+//     updateTask.querySelector("input").value =
+//       listItem.querySelector("p").innerHTML;
+//     function yes() {
+//       listItem.querySelector("p").innerHTML =
+//         updateTask.querySelector("input").value;
+//       toastMsg("Tasks is successfully updated");
+//       updateTask.style.display = "none";
+//       document.body.style.overflow = "auto";
+//       updateTask
+//         .querySelectorAll("button")[0]
+//         .removeEventListener("click", yes);
+//       updateTask.querySelectorAll("button")[1].removeEventListener("click", no);
+//     }
+//     function no() {
+//       updateTask.style.display = "none";
+//       document.body.style.overflow = "auto";
+//       updateTask
+//         .querySelectorAll("button")[0]
+//         .removeEventListener("click", yes);
+//       updateTask.querySelectorAll("button")[1].removeEventListener("click", no);
+//     }
+//     updateTask.querySelectorAll("button")[0].addEventListener("click", yes);
+//     updateTask.querySelectorAll("button")[1].addEventListener("click", no);
+//     updateTask.querySelector("i").addEventListener("click", function () {
+//       updateTask.style.display = "none";
+//       document.body.style.overflow = "auto";
+//     });
+//   });
+// }
 
 const showaddmsg = () => {
-    addToast.classList.add('show');
-    addToast.querySelector('.progress').classList.add('active');
-    setTimeout(() => {
-        addToast.classList.remove('show');
-    }, 2000);
-    setTimeout(() => {
-        addToast.querySelector('.progress').classList.remove('active');
-    }, 2500);
-}
+  addToast.classList.add("show");
+  addToast.querySelector(".progress").classList.add("active");
+  setTimeout(() => {
+    addToast.classList.remove("show");
+  }, 2000);
+  setTimeout(() => {
+    addToast.querySelector(".progress").classList.remove("active");
+  }, 2500);
+};
 
 const showupdatemsg = () => {
-    updateToast.classList.add('show');
-    updateToast.querySelector('.progress').classList.add('active');
-    setTimeout(() => {
-        updateToast.classList.remove('show');
-    }, 2000);
-    setTimeout(() => {
-        updateToast.querySelector('.progress').classList.remove('active');
-    }, 2500);
-}
+  updateToast.classList.add("show");
+  updateToast.querySelector(".progress").classList.add("active");
+  setTimeout(() => {
+    updateToast.classList.remove("show");
+  }, 2000);
+  setTimeout(() => {
+    updateToast.querySelector(".progress").classList.remove("active");
+  }, 2500);
+};
 
 const showdeletemsg = () => {
-    deleteToast.classList.add('show');
-    deleteToast.querySelector('.progress').classList.add('active');
-    setTimeout(() => {
-        deleteToast.classList.remove('show');
-    }, 2000);
-    setTimeout(() => {
-        deleteToast.querySelector('.progress').classList.remove('active');
-    }, 2500);
-}
+  deleteToast.classList.add("show");
+  deleteToast.querySelector(".progress").classList.add("active");
+  setTimeout(() => {
+    deleteToast.classList.remove("show");
+  }, 2000);
+  setTimeout(() => {
+    deleteToast.querySelector(".progress").classList.remove("active");
+  }, 2500);
+};
 
 (function () {
-    const lsalltodos = JSON.parse(localStorage.getItem("todos"));
-    if (lsalltodos !== null) {
-        lsalltodos.forEach((todos) => {
-            addTodo(todos);
-        })
-    }
+  const lsalltodos = JSON.parse(localStorage.getItem("todos"));
+  if (lsalltodos !== null) {
+    lsalltodos.forEach((todos) => {
+      addTodo(todos);
+    });
+  }
 })();
